5bc3a8325a1b263723fa02c5b90d92b56ac37ad0
Zhi <freemanzheng@hotmail.com>
2026-06-21 21:14:44 +0800

    fix(router): restore useActionData/useLoaderData exports

--- packages/router/src/mod.ts
+++ packages/router/src/mod.ts
@@ -1,15 +1,7 @@
-export { defineRoutes } from './define-routes.ts';
-export type { RouteConfig, RouteMeta } from './define-routes.ts';
-export { toHono, toURLPattern } from './pattern-translate.ts';
-export { Router } from './client-router.ts';
-export type { RouterStartOptions } from './client-router.ts';
-export { loadPage } from './page-loader.ts';
-export type { PageData } from './page-loader.ts';
-export { normalizeLocalePath } from './locale-path.ts';
-export type { LocalePath } from './locale-path.ts';
-export { useActionData, useLoaderData } from './use-loader-data.ts';
+export { normalizeLocalePath } from '@openelement/protocol';
+export type { LocalePath } from '@openelement/protocol';
+export { useActionData, useLoaderData } from './data-context.ts';
 export {
-  __internal_clearActionData,
   __internal_setActionData,
   __internal_setLoaderData,
 } from './data-context.ts';
